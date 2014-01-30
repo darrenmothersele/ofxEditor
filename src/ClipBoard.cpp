8b78ff128612f4abcdf2b38846bce139045d4976
Darren Mothersele <darren@darrenmothersele.com>
2014-01-30 16:43:29 +0000

    Update ClipBoard.cpp

--- src/ClipBoard.cpp
+++ src/ClipBoard.cpp
@@ -1,10 +1,22 @@
-//
-//  Clipboard.cpp
-//  textEditor
-//
-//  Created by Darren Mothersele on 19/11/2013.
-//
-//
+/*
+* Copyright (C) 2012 Akira Hayasaka & Dan Wilcox <danomatika@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*
+* See https://github.com/Akira-Hayasaka/ofxGLEditor for more info.
+*/
 
 #include "Clipboard.h"
 
